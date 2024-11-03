4ebc1d002895f0673abc4e897ca2acc70af68d90
Sufyan Ahmed <sufyanahmedmughal01.@gmail.com>
2024-11-03 09:44:20 -0800

    Road Adding
    Build the Functions for Adding Road in the Network.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,98 @@
-#include "Add Road/AddRoadClass.h"
-#include "Menu/Menu.h"
-#include "Road Network/RoadNetworkClass.h" // Include the header file
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    RoadNetwork network(100);
-    int choice;
-    while (true) {
-        showMenu();
+// R O A D - C L A S S
+class Road
+{
+public:
+    int id;
+    int start;
+    int end;
+    int length;
+    int capacity;
+    int currentCars;
+
+    Road(int id = -1, int start = -1, int end = -1, int length = 0, int capacity = 0)
+    {
+        this->id = id;
+        this->start = start;
+        this->end = end;
+        this->length = length;
+        this->capacity = capacity;
+        this->currentCars = 0;
+    }
+};
+
+// M A I N - C L A S S
+class RoadNetwork
+{
+public:
+    int numIntersections;
+    vector<vector<Road>> adjList; // Adjacency list for roads
+
+    RoadNetwork(int numIntersections) : numIntersections(numIntersections)
+    {
+        adjList.resize(numIntersections);
+    }
+
+    ~RoadNetwork()
+    {
+    }
+
+    // Function for Adding Road
+    void addRoad(int roadId, int from, int to, int length, int capacity)
+    {
+        Road road(roadId, from, to, length, capacity);
+        adjList[from].push_back(road);
+        adjList[to].push_back(Road(roadId, to, from, length, capacity));
+    }
+};
+
+// M E N U
+void showMenu()
+{
+    cout << "\n--- Traffic Management Menu ---\n";
+    cout << "1. Add Road\n";
+    cout << "Choose an option: ";
+}
+
+int main()
+{
+    int numIntersections;
+    cout << "Enter the number of intersections in the road network: ";
+    cin >> numIntersections;
+    RoadNetwork network(numIntersections); // Initialize network with number of intersections
+
+    int choice; // Variable for getting user choice
+
+    while (true)
+    {
+        showMenu(); // Call the menu function
         cin >> choice;
 
-        switch (choice) {
-        case 1: {
+        switch (choice)
+        {
+        case 1:
             int roadId, from, to, length, capacity;
-            cout << "Add Road Id: ";
+            cout << "Enter road ID: ";
             cin >> roadId;
-            cout << "Road From Intersection: ";
+            cout << "Enter start intersection: ";
             cin >> from;
-            cout << "Road End Intersection: ";
+            cout << "Enter end intersection: ";
             cin >> to;
-            cout << "Road Length: ";
+            cout << "Enter road length: ";
             cin >> length;
-            cout << "Road Cars Capacity: ";
+            cout << "Enter road capacity: ";
             cin >> capacity;
             network.addRoad(roadId, from, to, length, capacity);
+            cout << "Road added successfully.\n";
             break;
-        }
         default:
             cout << "Invalid option. Please try again.\n";
         }
     }
-    
+
     return 0;
 }
